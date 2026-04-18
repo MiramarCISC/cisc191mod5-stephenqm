8e9c7597ed093c7bb36a618b15c4c54012bb6f3e
jocelynnguyen <jnguyen118@stu.sdccd.edu>
2026-04-17 19:50:47 -0700

    bracket node fix

--- src/main/java/edu/sdccd/cisc191/GameAlgorithms.java
+++ src/main/java/edu/sdccd/cisc191/GameAlgorithms.java
@@ -170,12 +170,12 @@ public class GameAlgorithms {
             return false;
         }
 
-        if (node.matchName().equals(target)) {
+        if (node.matchName.equals(target)) {
             return true;
         }
 
-        return containsMatchHelper(node.left(), target)
-                || containsMatchHelper(node.right(), target);
+        return containsMatchHelper(node.left, target)
+                || containsMatchHelper(node.right, target);
     }
 
     /**
